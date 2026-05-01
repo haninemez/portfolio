aaef15b4f091b9745aa403ec3a823370fb3a2d2e
2moovin <ahmedmez94320@gmail.com>
2026-05-01 16:41:36 +0200

    thi

--- src/components/sections/PersonalSection.tsx
+++ src/components/sections/PersonalSection.tsx
@@ -22,7 +22,7 @@ export default function PersonalSection() {
         {personalByYear.map(({ year, projects }, groupIndex) => (
           <div key={year}>
             {groupIndex > 0 && <div className="mt-16" />}
-            <h3 className="font-display text-5xl font-black text-gray-200 md:text-7xl lg:text-8xl mb-8 select-none">
+            <h3 className="font-display text-5xl font-black text-[#918cc5] md:text-7xl lg:text-8xl mb-8 select-none">
               {year}
             </h3>
             <div className="space-y-24">
